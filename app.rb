8ee36da5b9d7e807bdb9af56d3394cc6085f900e
Ivan Lučev <lucev.ivan@gmail.com>
2013-07-20 01:12:40 +0200

    Add Post and basic methods

--- app.rb
+++ app.rb
@@ -2,12 +2,56 @@ require 'rubygems'
 require 'bundler'
 
 require 'sinatra'
+require 'data_mapper'
+require 'dm-timestamps'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
 
 get '/' do
-  "Hello World!"
+  @posts = Post.all
+  haml :"posts/index"
 end
 
-get '/*' do
-  status 404
-  body "404 page doesn't exist"
+get '/posts/new' do
+  @title = "New post"
+  haml :"posts/new"
+end
+
+post '/posts/create' do
+  @post = Post.create(:title => params[:title], :slug => params[:slug], :content => params[:content])
+  redirect '/'
+end
+
+get '/:slug' do
+  @post = Post.first(:slug => "#{params[:slug]}")
+  if(!@post.nil?)
+    haml :"posts/show"
+  else
+    status 404
+    body "404 page doesn't exist"
+  end
+end
+
+class Post
+  include DataMapper::Resource
+
+  property  :id,          Serial
+  property  :title,       String
+  property  :slug,        String
+  property  :content,     String
+  property  :created_at,  DateTime
+
+  validates_uniqueness_of :slug
+
+  DataMapper.auto_upgrade!
+end
+
+helpers do
+
+  def link_to(url,text=url,opts={})
+    attributes = ""
+    opts.each { |key,value| attributes << key.to_s << value << "\" "}
+    "<a href=\"#{url}\" #{attributes}>#{text}</a>"
+  end
+
 end
