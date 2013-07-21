8e170ef54474ac8ff8ad028d77dd0265e67f84b0
Ivan Lučev <lucev.ivan@gmail.com>
2013-07-21 14:51:24 +0200

    Add spec for Post validation

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,28 +1,16 @@
 require 'spec_helper'
 
-RSpec.configure do |config|
-  config.include Rack::Test::Methods
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/test.db")
-  DataMapper.finalize
-  Post.auto_migrate!
-end
-
-describe 'Sinatra App' do
-  it 'should respond to GET' do
-    get '/'
-    last_response.should be_ok
+describe "post" do
+  
+  it "should require title" do
+    post = Post.new
+    post.should_not be_valid
+    post.errors[:title].first.should match "Title must not be blank"
   end
-end
 
-describe "post /posts/create" do
-  it "should insert the post in the database" do
-    lambda do
-      post "/posts/create", params = {
-        :title => 'title',
-        :body => 'body'
-      }
-    end.should {
-      change(Post, :count).by(1)
-    }
+  it "should require slug" do
+    post = Post.new
+    post.should_not be_valid
+    post.errors[:slug].first.should match "Slug must not be blank"
   end
 end
